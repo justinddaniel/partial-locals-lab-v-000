39462ae4eb08b421aea63ebef951c805dd0370ff
Justin Daniel <justinddaniel@gmail.com>
2017-11-20 21:33:50 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,13 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def search(student_name = nil)
+    if student_name == nil
+      Student.all 
+    else
+      Student.find_by(:name => :student_name)
+    end
+  end
+  
 end
